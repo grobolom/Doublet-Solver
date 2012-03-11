b747abb06a9acff14e72bec1137a5359a2fdd893
Vasja Volin <vasja@superprofundo.com>
2012-03-10 18:19:52 -0800

    added function to check if a word is one letter away from another

--- wordladder.py
+++ wordladder.py
@@ -4,4 +4,16 @@ endword = 'GOLF'
 filename = '/usr/share/dict/words'
 words = [x.upper().strip() for x in open(filename) if len(x.upper().strip()) == len(startword)]
 
+def oneletteroff(a,b):
+	one = 0
+	for i in range(len(a)):
+		if a[i] != b[i]:
+			one += 1
+		if one > 1:
+			return None
+	return 1
 
+def oneaway(word, all):
+	return [x for x in all if oneletteroff(word, x)]
+
+print oneaway(startword, words)
