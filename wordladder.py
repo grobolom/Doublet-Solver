1c3e441cf7360485edfa0158279476d56e65e252
Vasja Volin <vasja@superprofundo.com>
2012-03-12 14:52:13 -0700

    added a list of words to be used that contains inflections (tent + tents) in addition to using the default unix set.

--- wordladder.py
+++ wordladder.py
@@ -1,9 +1,15 @@
 from collections import defaultdict
+import urllib2
 
-def allwordsoflength(l):
-	filename = '/usr/share/dict/words'
-	words = set([x.upper().strip() for x in open(filename) if len(x.upper().strip()) == l])
-	return words
+def allwords():
+	filename = '2of12inf.txt'
+	filename2 = '/usr/share/dict/words'
+	words = [x for x in open(filename)]	
+	words += [x for x in open(filename2)]
+	return words 
+
+def allwordsoflength(l, all):
+	return set([x.upper().strip() for x in all if len(x.upper().strip()) == l])
 
 def wildcard(s, idx):
     return s[:idx] + '?' + s[idx+1:]
@@ -16,7 +22,7 @@ def buildindex(all):
 	index = defaultdict(list)
 	for w in all:
 		for wild in wildcarded(w):
-			index[wild].append(w)
+			index[wild.upper()].append(w)
 	return index
 
 def oneaway(word, index):
@@ -25,11 +31,11 @@ def oneaway(word, index):
 		ret += index[w]
 	return ret
 
-def search(start, end, maxdepth = 20):
+def search(start, end, all, maxdepth = 10):
 	ftree = [(start, 0, [])]
 	done = [start]
-	all = allwordsoflength(len(start))
-	index = buildindex(all)
+	alloflen = allwordsoflength(len(start), all)
+	index = buildindex(alloflen)
 
 	for e in ftree:
 		if e[1] > maxdepth:
@@ -37,6 +43,7 @@ def search(start, end, maxdepth = 20):
 
 		children = oneaway(e[0], index)
 		children = list(set(children) - set(done))
+
 		if children == [] and e == ftree[-1]:
 			return 'Search Exhausted.' 
 
@@ -48,11 +55,13 @@ def search(start, end, maxdepth = 20):
 		
 		done = list(set(done) | set(children))
 
-print search('PIG','STY')
-print search('OAT','RYE')
-print search('CHIN','NOSE')
-print search('PITY','GOOD')
-print search('PITCH','TENTS')
-print search('FLOUR','BREAD')
-print search('COLD','WARM')
+
+all = allwords()
+print search('PIG','STY', all)
+print search('OAT','RYE', all)
+print search('CHIN','NOSE', all)
+print search('PITY','GOOD', all)
+print search('PITCH','TENTS', all)
+print search('FLOUR','BREAD', all)
+print search('COLD','WARM', all)
 	
